189e706a016084c1e92df29aedeb6e35293b58b2
brobinson21 <brandonrobinson0000@gmail.com>
2024-02-05 18:38:06 -0500

    add fetch for users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 
-function UsersPage() {
-  return (
-    <div>
-      Hello World
-    </div>
+interface User {
+    id: number;
+    name: string;
+    email: string;
+
+}
+
+async function UsersPage() {
+const res = await fetch('https://jsonplaceholder.typicode.com/users')
+const users: User[] = await res.json();
+
+
+
+  return (<><h1>Users</h1>
+
+  <table className='table table-bordered'>
+  <thead>
+    <tr>
+        <td>Name</td>
+        <td>Email</td>
+    </tr>
+    
+    </thead> 
+    <tbody>
+    {users.map((user)=><tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        </tr>)}
+
+    </tbody>
+    </table>
+
+  </>
+  
   )
 }
 
