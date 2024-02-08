56f40f7135d5ce12a116e5420d865dbfbaf4bcdb
brobinson21 <brandonrobinson0000@gmail.com>
2024-02-08 18:24:09 -0500

    add prisma

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import schema from "../schema";
 
 export function GET(request: NextRequest, { params }: { params: { id: number } }) {
     
@@ -9,4 +9,27 @@ export function GET(request: NextRequest, { params }: { params: { id: number } }
     }
     
     return NextResponse.json({id:1, name: 'Brandon'})
+}
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+    const body = await request.json()
+    const validation = schema.safeParse(body)
+     if (!validation.success) {
+         return NextResponse.json(validation.error.errors, {status: 400})
+     }
+     if (params.id > 10) {
+     return NextResponse.json({error:'user not found'}, {status:404})
+ }
+ 
+     
+     
+     return NextResponse.json({id:1, name:body.name})
+}
+ 
+export function DELETE(request: NextRequest, { params }: { params: { id: number } }) {
+    
+
+    if (params.id > 10) {
+        return NextResponse.json({error: 'user not found'}, {status:404})
+    }
+    return NextResponse.json({})
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-
-
-export function GET(request: NextRequest) {
-    return NextResponse.json(['data'])
+import schema from './schema'
+import prisma from '@/prisma/client'
+export async function GET(request: NextRequest) {
+    const users = await prisma.user.findMany()
+    
+    return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest) {
-    const body = await request.json() 
-    if(!body.name){return NextResponse.json({error: 'Name is required' })}
+    const body = await request.json()
+    const validation = schema.safeParse(body)
+    if(!validation.success){return NextResponse.json(validation.error.errors)}
     return NextResponse.json({id: 1, name: body.name })
 }
+
